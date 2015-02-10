62e9204ea44179742215f79c15f1091b05af6603
Csaba Dery <csaba.dery@gmail.com>
2015-02-09 22:48:08 -0500

    Added test project. Refactored code.

---
+++ SInterpreter.Tests/CodeTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using SInterpreter;
+
+namespace SInterpreter.Tests
+{
+    [TestClass]
+    public class CodeTest
+    {
+        [TestMethod]
+        public void TestCodeSnippets()
+        {
+            Interpreter interpreter = new Interpreter();
+
+            using (StreamReader codeReader = new StreamReader(File.Open("code2.txt", FileMode.Open)), 
+                                resultsReader = new StreamReader(File.Open("test2.txt", FileMode.Open))) {
+
+                Scanner scanner = new Scanner(codeReader);
+
+                Expression expression = scanner.NextExpression;
+                String expectedResult = resultsReader.ReadLine();
+                string actualResult = null;
+                while (expression != null)
+                {
+                    try
+                    {
+                        actualResult = interpreter.Evaluate(expression).ToString();
+                         
+                        Console.WriteLine(actualResult);
+                        if (!expectedResult.StartsWith("ignore"))
+                        {
+                            bool areEqual = AreResultsEqual(actualResult, expectedResult);
+                            if (!areEqual)
+                            {
+                                int i = 1;
+                            }
+                            Assert.IsTrue(areEqual);
+                        }
+                        else 
+                        {                       
+                            Console.WriteLine("Expected Printout: " + expectedResult.Replace("ignore", ""));
+                        }
+                    }
+                    catch (InterpreterException ex)
+                    {
+                        //Assert.Fail(string.Format("Error evaulating: {0} - {1}", expression.ToString()), ex.Message);
+                    }
+
+                    expression = scanner.NextExpression;
+                    expectedResult = resultsReader.ReadLine();
+                }
+            }
+        }
+
+        private bool AreResultsEqual(string evaluationResult, string testResult)
+        {
+            if (testResult == null)
+            {
+                return false;
+            }
+            double x, y;
+            if (double.TryParse(evaluationResult, out x) && double.TryParse(testResult, out y))
+            {
+                return Math.Round(x, 10) == Math.Round(y, 10);
+            }
+
+            return (evaluationResult.ToLower() == testResult.ToLower().Trim());
+        }
+
+    }
+}
---
+++ SInterpreter/Interpreter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SInterpreter.Native;
+
+namespace SInterpreter
+{
+    public class Interpreter
+    {
+        private Scanner _scanner;
+        private Frame _global;
+        private Frame _running;
+
+        public Interpreter()
+        {
+            _global = CreateGlobalFrame();
+            _running = new Frame(null, _global, null, null, null);
+        }
+
+        public object Evaluate(Expression expression)
+        {
+            return _running.Evaluate(expression);
+        }
+
+        public void run()
+        {
+            String line = Console.ReadLine();
+            while (line != null)
+            {
+                line = Console.ReadLine();
+            }
+        }
+
+        private Frame CreateGlobalFrame()
+        {
+            Dictionary<String, Procedure> globalBindings = new Dictionary<string, Procedure>();
+            Frame global = new Frame(globalBindings, null, null, new Identity(null, "0"), null); //TODO replace this with an eval proc    
+
+            global.AddBinding("+", new Add(global));
+            global.AddBinding("*", new Multiply(global));
+            global.AddBinding("-", new Subtract(global));
+            global.AddBinding("/", new Divide(global));
+            global.AddBinding(">", new GreaterThan(global));
+            global.AddBinding("<", new LessThan(global));
+            global.AddBinding(">=", new GreaterOrEqualThan(global));
+            global.AddBinding("<=", new LessOrEqualThan(global));
+            global.AddBinding("=>", new GreaterOrEqualThan(global));
+            global.AddBinding("=<", new LessOrEqualThan(global));
+            global.AddBinding("=", new Equals(global));
+            global.AddBinding("not", new Not(global));
+            global.AddBinding("remainder", new Remainder(global));
+            global.AddBinding("random", new SInterpreter.Native.Random(global));
+            global.AddBinding("true", new SInterpreter.Native.Boolean(global, true));
+            global.AddBinding("false", new SInterpreter.Native.Boolean(global, false));
+            global.AddBinding("display", new Display(global));
+            global.AddBinding("runtime", new Runtime(global));
+            global.AddBinding("newline", new Newline(global));
+            global.AddBinding("#t", new SInterpreter.Native.Boolean(global, true));
+            global.AddBinding("#f", new SInterpreter.Native.Boolean(global, false));
+            global.AddBinding("sin", new Sine(global));
+            global.AddBinding("cos", new Cosine(global));
+            global.AddBinding("log", new Logarithm(global));
+            global.AddBinding("expt", new Power(global));
+            global.AddBinding("floor", new Floor(global));
+            global.AddBinding("load", new Load(global));
+            global.AddBinding("cons", new Cons(global));
+            global.AddBinding("car", new Car(global));
+            global.AddBinding("cdr", new Cdr(global));
+            global.AddBinding("gcd", new GCD(global));
+            global.AddBinding("min", new Min(global));
+            global.AddBinding("max", new Max(global));
+            global.AddBinding("error", new Error(global));
+            global.AddBinding("list", new ListCreate(global));
+            global.AddBinding("null?", new NullCheck(global));
+
+            return global;
+        }
+    }
+}
